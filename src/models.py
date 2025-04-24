cb1fe99b2dd882201d25629b0df9bd0c3b072219
Evens Antoine <evens7antoine@gmail.com>
2025-04-24 01:12:51 +0000

    Need to do Fav serialize and test tables

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -16,4 +16,50 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    eye_color: Mapped[str] = mapped_column(nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(10), nullable=False)
+    character_favorites: Mapped["Favorites"] = relationship(back_populates = "favorites_characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year
+        }
+    
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
+    population: Mapped[str] = mapped_column(String(100000000000), nullable=False)
+    planet_favorites: Mapped["Favorites"] = relationship(back_populates = "favorites_planets")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "population": self.population
+        }
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    character_id: Mapped[int] = mapped_column(ForeignKey=("Characters.id"))
+    planet_id: Mapped[int] = mapped_column(ForeignKey=("Planets.id"))
+    favorites_characters: Mapped[list["Characters"]] = relationship(back_populates = "character_favorites")
+    favorites_planets: Mapped[list["Planets"]] = relationship(back_populates = "planet_favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year
         }
